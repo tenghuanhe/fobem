0e2cd662cb5156e0bac08eff1898c518b3c333b6
Tenghuan <tenghuanhe@gmail.com>
2015-10-02 22:51:08 +0800

    bg1

--- main.py
+++ main.py
@@ -1,46 +1,57 @@
 import time
-from multiprocessing import Process, Event
+from multiprocessing import Process, Event, Lock
 
 from pyemotiv import Epoc
 
 from pyfob import Fob
 
+winl = 20
 
-def Emotiv(e):
+
+def emotiv(e, l):
     epoc = Epoc()
     e.wait()
+
     t1 = time.time()
-    t2 = time.time()
+
     i = 0
-    while t2 - t1 < 20:
-        t2 = time.time()
+
+    while time.time() - t1 < winl:
         data = epoc.get_raw()
-        i = i + len(data[1])
-        print 'emotiv, ', i / 128.0
+        i += len(data[1])
+        l.acquire()
+        print 'Emotiv,    ', i / 128.0
+        print time.time()
+        l.release()
 
 
-def Ascension(e):
+def ascension(e, l):
     fs = 100.0
-    winl = 20.0
     fob = Fob()
     e.set()
+
     t0 = time.time()
     tp = time.time()
 
     i = 0
+
     while time.time() - t0 < winl:
         ti = time.time()
         if ti - tp >= (1.0 / fs):
             tp = time.time()
             fob.get_posang()
-            i = i + 1
-            print 'ascension, ', i / fs
+            i += 1
+            l.acquire()
+            print 'Ascension, ', i / fs
+            print time.time()
+            l.release()
     fob.close()
 
 
 if __name__ == '__main__':
     event = Event()
-    p1 = Process(target=Emotiv, args=(event,))
-    p2 = Process(target=Ascension, args=(event,))
+    lock = Lock()
+    p1 = Process(target=emotiv, args=(event, lock,))
+    p2 = Process(target=ascension, args=(event, lock,))
     p1.start()
     p2.start()
